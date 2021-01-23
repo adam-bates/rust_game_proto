f7b3ac8c3538e7787b11eae4096cc999980fc02b
Adam Bates <adam@adambates.ca>
2021-01-23 17:44:10 -0500

    Merged the 2 loops into 1 in update_draw_param_system.

--- src/game/ecs/systems/update_draw_param_system.rs
+++ src/game/ecs/systems/update_draw_param_system.rs
@@ -15,6 +15,7 @@ impl<'a> specs::System<'a> for UpdateDrawParamSystem {
     );
 
     fn run(&mut self, (camera_r, current_position_c, mut drawable_c): Self::SystemData) {
+        // Move drawable to current_position
         for (current_position, drawable) in (&current_position_c, &mut drawable_c).join() {
             // Help linter
             #[cfg(debug_assertions)]
@@ -23,22 +24,9 @@ impl<'a> specs::System<'a> for UpdateDrawParamSystem {
             let drawable = drawable as &mut Drawable;
 
             drawable.draw_params = drawable.draw_params.dest([
-                current_position.x * config::TILE_PIXELS_SIZE_F32,
-                current_position.y * config::TILE_PIXELS_SIZE_F32,
+                (current_position.x - camera_r.x) * config::TILE_PIXELS_SIZE_F32,
+                (current_position.y - camera_r.y) * config::TILE_PIXELS_SIZE_F32,
             ]);
-        }
-
-        for drawable in (&mut drawable_c).join() {
-            // Help linter
-            #[cfg(debug_assertions)]
-            let drawable = drawable as &mut Drawable;
-
-            if let ggez::graphics::Transform::Values { ref mut dest, .. } =
-                drawable.draw_params.trans
-            {
-                dest.x -= camera_r.x * config::TILE_PIXELS_SIZE_F32;
-                dest.y -= camera_r.y * config::TILE_PIXELS_SIZE_F32;
-            }
         }
     }
 }
